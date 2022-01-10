b40460bd4fb34f1cb30f867a102baa7347a10b5a
lubicazidekova <zidekova16@stud.uniza.sk>
2022-01-10 19:06:22 +0100

    Welcome page done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,30 @@
 @extends('layouts.app')
 @section('content')
-    <body class="books">
-    <div class="bg-text">
-        <h1>BOOK & MOVIE Blog</h1>
+<body class="BG-IMAGE_MAIN " id="imageID">
 
-    </div>
+<div class="bg-text">
+    <h1>BOOK & MOVIE Blog</h1>
+</div>
 
-    </body>
-    </html>
+
+<script>
+    let backgroundImagesArray = [
+        'css/images/movies.jpg',
+        'css/images/harley.jpg',
+        'css/images/popcorn.jpg',
+        'css/images/knihynapolici.jpg'
+    ]
+
+    let imageID = document.getElementById("imageID");
+
+    let i = 0;
+    setInterval(function() {
+        imageID.style.backgroundImage = "url(" + backgroundImagesArray[i] + ")";
+        i = i + 1;
+        if (i === backgroundImagesArray.length) {
+            i =  0;
+        }
+    }, 5000);
+
+</script>
 @endsection
