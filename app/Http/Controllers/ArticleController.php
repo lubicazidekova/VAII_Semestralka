efc5b8c73476e953501057a16392589ccb514a87
lubicazidekova <zidekova16@stud.uniza.sk>
2022-01-11 15:32:37 +0100

    Article CONTROLLER

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('article.index',[
+            'articles'=> Article::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('article.create', ['action' => route('article.store'), 'method' => 'post']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    { //todo validation
+
+
+        $article = Article::create($request->all());
+        $article->save();
+        redirect()->route('article.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        return view('article.show',[
+            'article'=>$article
+
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Article $article)
+    {
+        return view('article.edit', [
+            'action' => route('article.update', $article->id),
+            'method' => 'put',
+            'model' => $article
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Article $article)
+    {//todo validation
+
+
+        $article->update($request->all());
+        return redirect()->route('article.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $article=Article::find($id);
+        $article->delete();
+        return redirect()->route('article.index');
+    }
+}
