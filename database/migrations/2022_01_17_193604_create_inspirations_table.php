763a87bfbc0ddae61f68a890a5a8a260a9f53084
lubicazidekova <zidekova16@stud.uniza.sk>
2022-01-17 21:52:16 +0100

    Inspirations

---
+++ database/migrations/2022_01_17_193604_create_inspirations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInspirationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inspirations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->string('title',150);
+            $table->string('type');
+            $table->string('genre');
+            $table->string('image');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inspirations');
+    }
+}
