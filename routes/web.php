794082e70e0ede371bd0d2f864a676399fa4134c
lubicazidekova <zidekova16@stud.uniza.sk>
2022-01-17 16:44:48 +0100

    Routes adding

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ReviewController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,31 +21,67 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
+//WELCOME PAGE ROUTE
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-Route::get('/about', function () {
-    return view('contacts/about');
 
-});
+//INSPIRATION PAGE ROUTE
 Route::get('/inspiration', function () {
     return view('inspiration/inspiration');
+})->name('inspiration');
 
-});
+//EVENTS PAGE ROUTE
 Route::get('/events', function () {
     return view('events/events');
-
 });
 
-Auth::routes();
+//ABOUT PAGE ROUTE
+Route::get('/about', function () {
+    return view('contacts/about');
+})->name('about');
 
+//__________________________________________________________________________________
+//ARTICLES PAGE ROUTE
+Route::resource('/articles', ArticleController::class);
+Route::get('/articles', [ArticleController::class, 'index'])->name('article.index');
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('article.show');
+
+Route::resource('comments',CommentController::class);
+Route::get('/comments', [CommentController::class, 'index'])->name('comment.index');
+Route::get('/comments/{id}', [CommentController::class, 'show'])->name('comment.show');
+
+Route::resource('reviews',ReviewController::class);
+Route::get('/reviews', [ReviewController::class, 'index'])->name('reviews.index');
+
+//________________________AUTHENTIFICATION ROUTES___________________________________
+Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::group(['middleware'=>['auth']], function () {
+
+        //USERS ROUTES
         Route::resource('user', UserController::class);
         Route::get('user/{user}/delete', [UserController::class, 'destroy'])->name('user.delete');
+
+        //CONTACTS ROUTES
         Route::resource('contacts', ContactController::class);
         Route::get('contacts/{id}/delete', [ContactController::class, 'destroy'])->name('contact.delete');
         Route::get('contacts/{id}/edit', [ContactController::class, 'edit'])->name('contact.edit');
         Route::get('contacts/{id}', [ContactController::class, 'update'])->name('contact.update');
-    });
+
+        //ARTICLES ROUTES
+        Route::get('articles/{id}/delete', [ArticleController::class, 'destroy'])->name('article.delete');
+        Route::get('articles/{id}/edit', [ArticleController::class, 'edit'])->name('article.edit');
+        Route::get('articles/{id}', [ArticleController::class, 'update'])->name('article.update');
+
+
+        //COMMENTS ROUTES
+
+        Route::get('comments/{id}/delete', [CommentController::class, 'destroy'])->name('comment.delete');
+        Route::get('comments/{id}/edit', [ArticleController::class, 'edit'])->name('comment.edit');
+        Route::get('comments/{id}', [ArticleController::class, 'update'])->name('comment.update');
+
+
+});
+
